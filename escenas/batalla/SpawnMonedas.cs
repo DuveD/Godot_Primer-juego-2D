84b1a2a8a892b04d6613463c8dbb1767031e6a87
DuveD <davidmarinme@gmail.com>
2025-11-24 09:05:46 +0100

    Añadido Nodo Moneda. Añadido Spawn de monedas a escena de Batalla. Eliminado timer de puntuación y conectado a recogida de monedas.

---
+++ escenas/batalla/SpawnMonedas.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using Godot.Collections;
+using Primerjuego2D.escenas.objetos;
+
+public partial class SpawnMonedas : Control
+{
+	// Señal "MonedaRecogida" para indicar que el jugador ha recogido una moneda.
+	[Signal]
+	public delegate void MonedaRecogidaEventHandler();
+
+	[Export] public PackedScene MonedaScene;
+
+	public override void _Ready()
+	{
+		Spawn();
+	}
+
+	public override void _Process(double delta)
+	{
+		if (!ExisteMoneda())
+			Spawn();
+	}
+
+	public bool ExisteMoneda()
+	{
+		var monedas = GetTree().CurrentScene.GetChildren().OfType<Moneda>();
+		return monedas.Count() > 0;
+	}
+
+	public void Spawn()
+	{
+		var moneda = MonedaScene.Instantiate<Moneda>();
+		moneda.Recogida += OnMonedaRecogida;
+
+		GetTree().CurrentScene.AddChild(moneda);
+
+		float x = (float)GD.RandRange(GlobalPosition.X, GlobalPosition.X + Size.X);
+		float y = (float)GD.RandRange(GlobalPosition.Y, GlobalPosition.Y + Size.Y);
+
+		moneda.Position = new Vector2(x, y);
+	}
+
+	public void OnMonedaRecogida()
+	{
+		// Emitimos la señal de que el jugador ha recogido una moneda.
+		EmitSignal(SignalName.MonedaRecogida);
+	}
+
+	public void DestruirMonedas()
+	{
+		IEnumerable<Moneda> monedas = GetTree().CurrentScene.GetChildren().OfType<Moneda>();
+		foreach (Moneda moneda in monedas)
+			moneda.QueueFree();
+	}
+}
---
+++ escenas/objetos/Moneda.cs
@@ -0,0 +1,46 @@
+using Godot;
+using Primerjuego2D.escenas.entidades.jugador;
+using Primerjuego2D.nucleo.utilidades.log;
+using static Primerjuego2D.nucleo.utilidades.log.LoggerJuego;
+
+namespace Primerjuego2D.escenas.objetos;
+
+[AtributoNivelLog(NivelLog.Info)]
+public partial class Moneda : Area2D
+{
+	// Señal "Recogida" para indicar que la moneda ha sido recogida por el jugador.
+	[Signal]
+	public delegate void RecogidaEventHandler();
+
+	private CollisionShape2D _CollisionShape2D;
+
+	private CollisionShape2D CollisionShape2D => _CollisionShape2D ??= GetNode<CollisionShape2D>("CollisionShape2D");
+
+	public override void _Ready()
+	{
+		LoggerJuego.Trace(this.Name + " Ready.");
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body is Jugador)
+		{
+			OnBodyEnteredJugador();
+		}
+	}
+
+	private void OnBodyEnteredJugador()
+	{
+		LoggerJuego.Trace("Moneda recogida.");
+
+		// Emitimos la señal de que el jugador ha recogido la moneda.
+		EmitSignal(SignalName.Recogida);
+
+		// Cuando el jugador recoja la moneda, la destruimos.
+		QueueFree();
+	}
+}
